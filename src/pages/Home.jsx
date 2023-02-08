0d37d84133e0b1ec477c5cd66dfd5c35029f4f08
ElenaS35 <helena0518savitskaya@gmail.com>
2023-02-08 14:33:58 +0200

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,18 +11,20 @@ const Home = () => {
         <>
             <Header />
             <main className="section">
-                <div className="container">
-                    <ul className="content-list">
-                        <li className="content-list__item">
-                            <h2 className="title-2">Frontend</h2>
-                            <p>HTML, CSS, SASS/SCSS</p>
-                            <p>JavaScript, TypeScript, ReactJS, Redux, BootStrap, Material UI, Figma</p>
-                        </li>
-                        <li className="content-list__item">
-                            <h2 className="title-2">Backend</h2>
-                            <p>NodeJS</p>
-                        </li>
-                    </ul>
+                <div className="main-content">
+                    <div className="container">
+                        <ul className="content-list">
+                            <li className="content-list__item">
+                                <h2 className="title-2">Frontend</h2>
+                                <p>HTML, CSS, SASS/SCSS</p>
+                                <p>JavaScript, TypeScript, ReactJS, Redux, BootStrap, Material UI, Figma</p>
+                            </li>
+                            <li className="content-list__item">
+                                <h2 className="title-2">Backend</h2>
+                                <p>NodeJS</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <Connect active={active} setActive={setActive} />
                 <Cert />
