2b73efd53f252ed444fece59d2c34e224c6f1b9d
ElenaS35 <helena0518savitskaya@gmail.com>
2023-02-09 09:59:14 +0200

    new

--- src/components/certificates/CertInfo.jsx
+++ src/components/certificates/CertInfo.jsx
@@ -2,10 +2,10 @@ import "./_certificate.scss"
 
 const CertInfo = ({ title, img, link }) => {
     return (
-        <li className="certificate">
+        <div className="certificate">
             <h3 className="certificate__title"><span>{title}</span></h3>
             <a href={link} target="_blank" rel="noreferrer" className="certificate__link"><img src={img} alt={title} className="certificate__img" /></a>
-        </li>
+        </div>
     );
 };
 
